257bfa2433fad0e573406a0c336c782b36d90d24
Hoobert <hfleclerc5@gmail.com>
2025-04-23 12:49:50 -0400

    Modifie MainActivity pour utiliser AppCompatActivity

---
+++ app/src/main/java/com/labpixel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.labpixel;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Assurez-vous que le layout est correctement configuré
+    }
+}
+
