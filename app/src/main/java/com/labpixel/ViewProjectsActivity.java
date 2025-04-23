ee73ea92bdb5b5cf86321e49709a5f4c692bcd70
Hoobert <hfleclerc5@gmail.com>
2025-04-23 15:12:05 -0400

    Ajout des activités EditImage, ViewProjects, Settings et leurs layouts XML

---
+++ app/src/main/java/com/labpixel/ViewProjectsActivity.java
@@ -0,0 +1,13 @@
+package com.labpixel;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProjectsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_projects);
+    }
+}
+
